72bb463c4543feaad75d58325a11804331e451c3
jjholt <jonathanjamesholt@gmail.com>
2025-07-03 02:10:38 +0100

    working snippet expansion

--- dotfiles/nvim/after/plugin/lsp.lua
+++ dotfiles/nvim/after/plugin/lsp.lua
@@ -1,41 +1,11 @@
 require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed = {
-        -- 'rust_analyzer',
+        'rust_analyzer',
         'texlab',
         'lua_ls',
         'matlab_ls',
     },
-    -- handlers = {
-    --     -- lsp.default_setup,
-    --     rust_analyzer=lsp.noop,
-    -- },
-})
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-cmp.setup({
-    mapping = {
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        -- ['<Tab>'] = cmp_action.luasnip_supertab(),
-        -- ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-        ['<C-s>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        -- ['<C-Space>'] = cmp.mapping.complete(),
-    },
-    sources = cmp.config.sources({
-        -- {name = "lsp-zero" },
-        { name = "nvim_lsp" },
-        { name = "nvim_lsp_signature_help" },
-        { name = "nvim_lua" },
-        { name = "luasnip" },
-        { name = "path" },
-    }, {
-        { name = "buffer", keyword_length = 3 },
-    }),
 })
 -- lsp.setup_nvim_cmp({
 --   mapping = cmp_mappings,
@@ -56,3 +26,4 @@ cmp.setup({
 vim.diagnostic.config({
     virtual_text = true
 })
+
--- dotfiles/nvim/after/plugin/luasnip.lua
+++ dotfiles/nvim/after/plugin/luasnip.lua
@@ -14,20 +14,51 @@ if not pcall(require, "luasnip") then
   return
 end
 
-vim.keymap.set({ "i", "s" }, "<c-k>", function()
-  if ls.expand_or_jumpable() then
-    ls.expand_or_jump()
-  end
-end, { silent = true })
 
-vim.keymap.set({ "i", "s" }, "<c-j>", function()
-  if ls.jumpable(-1) then
-    ls.jump(-1)
-  end
-end, { silent = true })
+-- vim.keymap.set({ "i", "s" }, "<Tab>", function()
+--   if ls.choice_active() then
+--     ls.change_choice(1)
+--   end
+-- end, { silent = true, desc = "Next choice" })
 
-vim.keymap.set({ "i", "s" }, "<Tab>", function()
-  if ls.choice_active() then
-    ls.change_choice(1)
-  end
-end, { silent = true, desc = "Next choice" })
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            ls.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        -- ["<Tab>"] = cmp_action.luasnip_supertab(),
+        -- ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+        ["<C-s>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-e>"] = cmp.mapping.complete(),
+        ["<C-k>"] = cmp.mapping(function()
+            if ls.expand_or_jumpable() then
+                ls.expand_or_jump(-1)
+            end
+        end, {"i", "s"}),
+        ["<C-j>"] = cmp.mapping(function()
+            if ls.locally_jumpable(-1) then
+                ls.jump(-1)
+            end
+        end, {"i", "s"}),
+    }),
+    sources = cmp.config.sources({
+        -- {name = "lsp-zero" },
+        { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "luasnip" },
+        -- { name = "nvim_lua" },
+        -- { name = "path" },
+    }, {
+        { name = "buffer", keyword_length = 3 },
+    }),
+})
